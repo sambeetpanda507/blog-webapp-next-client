426e4aceffcc0ff2875551e6a2757eeaada0c473
SAMBEET SEKHAR PANDA <sambeetpanda507@gmail.com>
2023-04-09 14:10:37 +0530

    Update NavLink.tsx

--- components/NavBar/NavLink.tsx
+++ components/NavBar/NavLink.tsx
@@ -11,7 +11,7 @@ const NavLink: FC<PropsType> = (props) => {
   const { name, path, isActive } = props;
   return (
     <li
-      className={`py-5 px-4 bg-green-200 ${
+      className={`py-5 px-4 ${
         isActive
           ? 'bg-neutral-800 border-b-4 border-white'
           : 'bg-neutral-900 border-transparent'
