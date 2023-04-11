cce76921929ff10069a45066df84fbd33f9bbee6
SAMBEET SEKHAR PANDA <sambeetpanda507@gmail.com>
2023-04-11 15:04:51 +0530

    put themeprovider in a component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,11 @@
 import '@/styles/globals.css';
 import Layout from '@/components/Layout';
 import type { AppProps } from 'next/app';
-import theme from '@/utils/theme';
-import { ThemeProvider } from '@mui/material/styles';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
